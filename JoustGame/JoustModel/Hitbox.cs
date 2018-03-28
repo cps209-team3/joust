6503b52740c7816befef31cc3d2d86c5df504f38
jfran956 <jfran956@students.bju.edu>
2018-03-27 20:42:55 -0400

    initial object model

---
+++ JoustGame/JoustModel/Hitbox.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoustModel
+{
+    public class Hitbox
+    {
+        public double width;
+        public double height;
+    }
+}
