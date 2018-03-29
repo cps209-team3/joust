2c9c7a67b0a910a9deb388dfc7c6a63ae1c25505
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-03-29 15:31:57 -0400

    Add files via upload

---
+++ JoustGame/JoustModel/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoustModel
+{
+    class GameController
+    {
+        public World world { get; set; }
+
+        public GameController()
+        {
+            world = new World();
+        }
+
+        public void Load(string fileName, string user)
+        {
+            // search for section in file according to username
+            // Get lives count
+            // Get score number
+            // Get level number
+
+        }
+        public void Save(string fileName)
+        {
+            // write username
+            // write score data under "score"
+            // write lives count
+            // write level number
+
+            // Sample save line:
+            // USERNAME, SCORE, LIVES, LEVEL
+            // NerdDestroyer9000, 999999, 3, 10
+
+        }
+    }
+}
