6f4f3e23c954a5b144ff1263ab3f342f002bef41
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-04-01 23:36:55 -0400

    Update Platform.cs

--- JoustGame/JoustModel/Platform.cs
+++ JoustGame/JoustModel/Platform.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace JoustModel
 {
-    public class Platform : WorldObject
+    public class Platform : WorldObject, Iserialization
     {
 
+        // Convert coords to one line string to be put into save file
+        public string Serialize()
+        {
+            return Convert.ToString(this.coords);
+        }
+
+        // Set coords to value read from file
+        public void Deserialize(string coords)
+        {
+            this.coords = coords;
+        }
     }
 }
