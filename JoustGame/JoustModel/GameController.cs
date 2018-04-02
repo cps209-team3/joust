6fc2d0b224209a66b9a6ff3495b2d98e035b6752
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-04-02 19:47:40 -0400

    Update GameController.cs

--- JoustGame/JoustModel/GameController.cs
+++ JoustGame/JoustModel/GameController.cs
@@ -41,6 +41,7 @@ namespace JoustModel
         public void Save_Default()
         {
             GameController game = new GameController();
+            string line;
             // populate world with objects
             string result = "Player: [1000, 3, 2, (300,600)] WorldObjects: [platforms: 1, (100,300), false], [Entities: 1, buzzard, (500,700), 3.6, 4.7]";
             line = game.Load("Save 2018-4-2-5-32-00");
@@ -51,6 +52,7 @@ namespace JoustModel
         public void Load_Default()
         {
             GameController game = new GameController();
+            string line2save;
             // the save file needs test data
             string result = "Player: [1000, 3, 2, (300,600)] WorldObjects: [platforms: 1, (100,300), false], [Entities: 1, buzzard, (500,700), 3.6, 4.7]";
             line2save = game.Save();
