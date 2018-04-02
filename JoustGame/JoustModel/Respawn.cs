8acdb7286ccb7c80790cc7a53b4567e02b08629f
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-04-02 07:29:14 -0400

    Update Respawn.cs

--- JoustGame/JoustModel/Respawn.cs
+++ JoustGame/JoustModel/Respawn.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace JoustModel
 {
-    public class Respawn : Platform
+    public class Respawn : Platform, ISerializable
     {
+        public string Serialize()
+        {
+            return string.Format("Respawn, {1}", this.coords);
+        }
 
+        // Set coords to value read from file
+        public void Deserialize(string coords)
+        {
+            this.coords = coords;
+        }
     }
 }
