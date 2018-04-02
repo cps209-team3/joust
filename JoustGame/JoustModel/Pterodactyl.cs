3a4c70ec71406306a614035c0df5709fc9c2f78e
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-04-01 23:52:00 -0400

    Update Pterodactyl.cs

--- JoustGame/JoustModel/Pterodactyl.cs
+++ JoustGame/JoustModel/Pterodactyl.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace JoustModel
 {
-    public class Pterodactyl : Enemy
+    public class Pterodactyl : Enemy, Iserialization
     {
         public override int Value { get; set; }
 
         public Pterodactyl()
         {
             Value = 1000;
+        }
+        
+        
+        //Serialization
+        public string Serialize()
+        {
+            return string.Format("Pterodactyl, {0}, {1}, {2}", this.coords, this.speed, this.angle);
+        }
+
+        public void Deserialize(string coords, string speed, string angle)
+        {
+            this.coords = coords; //convert to point
+            this.speed = Convert.ToDouble(speed);
+            this.angle = Convert.ToDouble(angle);
+
         }
     }
 }
