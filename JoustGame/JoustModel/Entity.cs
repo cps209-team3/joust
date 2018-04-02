17ba9265f35ced8ea62814dd26ec26c5cb5c30e5
jfran956 <jfran956@students.bju.edu>
2018-04-02 01:36:02 -0400

    added some unit tests

--- JoustGame/JoustModel/Entity.cs
+++ JoustGame/JoustModel/Entity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JoustModel
 {
@@ -12,5 +13,69 @@ namespace JoustModel
         public double angle;
         
         public abstract int Value { get; set; }
+
+        public void Die()
+        {
+            World.Instance.objects.Remove(this);
+            // Play death animation
+        }
+
+        public void Update()
+        {
+            // Move based on input/AI
+            // Check for collisions
+        }
+    }
+
+    [TestClass]
+    public class EntityTest
+    {
+        [TestMethod]
+        public void TestMoveUp()
+        {
+            Entity e = new Ostrich();
+            // set initial speed, angle, and position
+            e.Update();
+            Point testPoint = new Point();
+            testPoint.x = 450;
+            testPoint.y = 500;
+            Assert.AreEqual(testPoint, e.coords);
+        }
+
+        [TestMethod]
+        public void TestMoveLeft()
+        {
+            Entity e = new Ostrich();
+            // set initial speed, angle, and position
+            e.Update();
+            Point testPoint = new Point();
+            testPoint.x = 450;
+            testPoint.y = 500;
+            Assert.AreEqual(testPoint, e.coords);
+        }
+
+        [TestMethod]
+        public void TestMoveRight()
+        {
+            Entity e = new Ostrich();
+            // set initial speed, angle, and position
+            e.Update();
+            Point testPoint = new Point();
+            testPoint.x = 450;
+            testPoint.y = 500;
+            Assert.AreEqual(testPoint, e.coords);
+        }
+
+        [TestMethod]
+        public void TestMoveDown()
+        {
+            Entity e = new Ostrich();
+            // set initial speed, angle, and position
+            e.Update();
+            Point testPoint = new Point();
+            testPoint.x = 450;
+            testPoint.y = 500;
+            Assert.AreEqual(testPoint, e.coords);
+        }
     }
 }
--- JoustGame/JoustModel/Point.cs
+++ JoustGame/JoustModel/Point.cs
@@ -10,5 +10,11 @@ namespace JoustModel
     {
         public double x;
         public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
