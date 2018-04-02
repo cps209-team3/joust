fd5d0433f9b8ecb16e9c0a4091239fc83edc3918
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-04-01 23:52:46 -0400

    Update Buzzard.cs

--- JoustGame/JoustModel/Buzzard.cs
+++ JoustGame/JoustModel/Buzzard.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace JoustModel
 {
-    public class Buzzard : Enemy
+    public class Buzzard : Enemy, Iserialization
     {
         public override int Value { get; set; }
 
         public Buzzard()
         {
             Value = 500;
+        }
+        
+        //Serialization
+        public string Serialize()
+        {
+            return string.Format("Buzzard, {0}, {1}, {2}", this.coords, this.speed, this.angle);
+        }
+
+        public void Deserialize(string coords, string speed, string angle)
+        {
+            this.coords = coords; //convert to point
+            this.speed = Convert.ToDouble(speed);
+            this.angle = Convert.ToDouble(angle);
         }
     }
 }
