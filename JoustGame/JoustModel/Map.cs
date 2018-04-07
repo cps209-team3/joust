3ee63f4eb68f164f911d796587db6c53e1920d82
jfran956 <jfran956@students.bju.edu>
2018-04-06 22:31:49 -0400

    fix more errors

--- JoustGame/JoustModel/Map.cs
+++ JoustGame/JoustModel/Map.cs
@@ -5,23 +5,23 @@ using System.Linq;
 
 namespace JoustModel
 {
-    public class Map : ISerializable
+    public class Map// : ISerializable
     {
         public List<Platform> platforms = new List<Platform>();
         
         
-        public override string Serialize()
-        {
-            return string.Format("Map, {0}", this.numOfPlats, this.platCoords, this.numOfRespawns, this.respawnCoords);
-        }
+        //public override string Serialize()
+        //{
+        //    return string.Format("Map, {0}", this.numOfPlats, this.platCoords, this.numOfRespawns, this.respawnCoords);
+        //}
 
-        // Set coords to value read from file
-        public override void Deserialize(string data)
-        {
-            // set numOfPlats
-            // set platCoords
-            // set numOfRespawns
-            // set respawnCoords
-        }
+        //// Set coords to value read from file
+        //public override void Deserialize(string data)
+        //{
+        //    // set numOfPlats
+        //    // set platCoords
+        //    // set numOfRespawns
+        //    // set respawnCoords
+        //}
     }
 }
