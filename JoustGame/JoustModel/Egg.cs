9a3a817ee32951bfd2004197474884f74b07017b
jefti558 <32042489+jefti558@users.noreply.github.com>
2018-04-10 14:01:02 -0400

    Update Egg.cs

--- JoustGame/JoustModel/Egg.cs
+++ JoustGame/JoustModel/Egg.cs
@@ -31,7 +31,7 @@ namespace JoustModel
         //Serialization
         public override string Serialize()
         {
-            return string.Format("Egg,{0},{1},{2},{3},{4},{5}",speed, angle, coords.x, coords.y, acceleration, accelerationAngle);
+            return string.Format("Egg,{0},{1},{2},{3}",speed, angle, coords.x, coords.y);
         }
 
         public override void Deserialize(string data)
@@ -41,8 +41,6 @@ namespace JoustModel
             angle = Convert.ToDouble(properties[2]); // set angle
             coords.x = Convert.ToDouble(properties[3]); // set x coord
             coords.y = Convert.ToDouble(properties[4]); // set y coord
-            acceleration = Convert.ToDouble(properties[5]); // set acceleration
-            accelerationAngle = Convert.ToDouble(properties[6]); // set accelerationAngle
         }
     }
 
