108c98a95ab8a08bcdecb955bd2e43b37ccdab1b
jefti558 <jefti558@students.bju.edu>
2018-04-14 18:08:37 -0400

    rectangle

--- JoustGame/JoustModel/WorldObject.cs
+++ JoustGame/JoustModel/WorldObject.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 
 namespace JoustModel
 {
     public abstract class WorldObject : ISerializable
     {
         public Point coords = new Point(0,0);
-        public Hitbox hitbox;
+        //public Hitbox hitbox;
+        public Rectangle hitbox;
         public string imagePath;
         public string type;
+
 
         abstract public string Serialize();
         abstract public void Deserialize(string data);
