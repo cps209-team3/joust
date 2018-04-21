9b2bf4fb7eb5f1986c4243c63224c338677e1064
jfran956 <jfran956@students.bju.edu>
2018-04-21 01:01:59 -0400

    dead and spawn states

---
+++ JoustGame/JoustModel/DeadState.cs
@@ -0,0 +1,33 @@
+﻿namespace JoustModel
+{
+    public class DeadState : IState
+    {
+        StateMachine stateMachine;
+        Ostrich ostrich;
+
+        public DeadState(Ostrich ostrich)
+        {
+            this.ostrich = ostrich;
+            this.stateMachine = ostrich.stateMachine;
+        }
+
+        public void Update()
+        {
+            ostrich.nSpeed = 600;
+            ostrich.nAngle = 0;
+            if (ostrich.coords.x > 1500)
+            {
+                stateMachine.Change("spawn");
+            }
+        }
+
+        public void HandleInput(string command) { }
+        public void Enter() { }
+        public void Exit() { }
+
+        public override string ToString()
+        {
+            return "dead";
+        }
+    }
+}
---
+++ JoustGame/JoustModel/SpawnState.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+
+namespace JoustModel
+{
+    public class SpawnState : IState
+    {
+        StateMachine stateMachine;
+        Ostrich ostrich;
+
+        public SpawnState(Ostrich ostrich)
+        {
+            this.ostrich = ostrich;
+            this.stateMachine = ostrich.stateMachine;
+        }
+
+        public void Update() { }
+
+        public void HandleInput(string command) { }
+        public void Enter()
+        {
+            ostrich.coords.x = World.Instance.basePlatform.coords.x + 350;
+            ostrich.coords.y = World.Instance.basePlatform.coords.y;
+            ostrich.speed = 0.075;
+            ostrich.angle = 90;
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                ostrich.speed = 0;
+            });
+        }
+        public void Exit() { }
+
+        public override string ToString()
+        {
+            return "spawn";
+        }
+    }
+}
